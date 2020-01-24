20b14f3c0c7c1908890613504134ae2d9a9006ce
damiTacconi <40439262+damiTacconi@users.noreply.github.com>
2020-01-24 11:42:13 -0300

    user added (#1)

---
+++ src/main/java/net/avalith/elections/controller/UserController.java
@@ -0,0 +1,56 @@
+package net.avalith.elections.controller;
+
+import net.avalith.elections.entities.UserListResponse;
+import net.avalith.elections.entities.UserResponse;
+import net.avalith.elections.model.User;
+import net.avalith.elections.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("")
+    public UserResponse save(@Validated @RequestBody User user){
+
+        return this.userService.save(user);
+    }
+
+    @GetMapping("{id}")
+    public User findById(@PathVariable("id") String id){
+       return this.userService.findById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") String id){
+
+        this.userService.delete(id);
+    }
+
+    @PutMapping("{id}")
+    public void update(@Validated @RequestBody User user , @PathVariable("id") String id){
+
+        this.userService.update(user, id);
+    }
+
+    @GetMapping("")
+    public UserListResponse findAll(){
+
+        return this.userService.findAll();
+    }
+
+}
---
+++ src/main/java/net/avalith/elections/model/User.java
@@ -0,0 +1,39 @@
+package net.avalith.elections.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    private String id;
+
+    @NotNull(message = "Insert a valid dni")
+    @Column(unique = true)
+    private Integer dni;
+
+    @NotNull(message = "Insert a valid name")
+    @NotBlank
+    private String name;
+}
