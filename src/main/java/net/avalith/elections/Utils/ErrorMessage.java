990228af55c1fe8075919f9c42fab4adaed5a6eb
damiTacconi <40439262+damiTacconi@users.noreply.github.com>
2020-01-24 12:48:09 -0300

    Feature add candidate (#2)

--- src/main/java/net/avalith/elections/Utils/ErrorMessage.java
+++ src/main/java/net/avalith/elections/Utils/ErrorMessage.java
@@ -1,5 +1,7 @@
 package net.avalith.elections.Utils;
 
 public class ErrorMessage {
+
+ public static final String CANDIDATE_NOT_FOUND = "The candidate was not found";
     public static final String USER_NOT_FOUND = "The user was not found";
 }
