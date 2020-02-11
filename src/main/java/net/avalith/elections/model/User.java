41c8bed8755ddd9b43fb8dafa6a8d43d8095375e
Damian Tacconi <dami_tano_95@hotmail.com>
2020-02-11 16:05:13 -0300

    properties

--- src/main/java/net/avalith/elections/model/User.java
+++ src/main/java/net/avalith/elections/model/User.java
@@ -7,12 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
